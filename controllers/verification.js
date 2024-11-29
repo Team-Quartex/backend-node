fb2330dcadcbfd556126a491b1dfb9a0c7a2d519
Shana2002 <hansakaravi02@gmail.com>
2024-11-29 18:13:13 +0530

    fix bug

--- controllers/verification.js
+++ controllers/verification.js
@@ -37,18 +37,26 @@ export const decline=(req,res)=>{
   });
 }
 
-export const requestverify = (req,res) => {
+export const requestverify = (req, res) => {
   const token = req.cookies.accessToken;
   if (!token) return res.status(401).json("Not Logged in!");
 
   jwt.verify(token, "secretkey", (err, userInfo) => {
     if (err) return res.status(403).json("Token is not valid");
 
-    const q = `INSERT INTO verifiction_request (userId,description,ytlink,fblink,twitterlink) VALUES (?)`;
+    const q = `INSERT INTO verifiction_request (userId, description, ytlink, fblink, twitterlink) VALUES (?, ?, ?, ?, ?)`;
+    const values = [
+      userInfo.id,
+      req.body.desc,
+      req.body.yt,
+      req.body.fb,
+      req.body.tw,
+    ];
 
-    db.query(q, [userInfo.id,req.body.desc,req.body.yt,req.body.fb,req.body.tw], (err, data) => {
+    db.query(q, values, (err, data) => {
+      console.log(err);
       if (err) return res.status(500).json(err);
-      return res.status(200).json(data);
+      return res.status(200).json("Request verification has been created");
     });
   });
-}
+};
